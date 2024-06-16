d3dd38a6def79b496d4fddcdab821dff3e17a707
riwandy <riwandy@popbox.asia>
2024-06-16 20:46:30 +0700

    change utils

--- src/utils/responseHandler.ts
+++ src/utils/responseHandler.ts
@@ -1,5 +1,37 @@
+import { Response } from "express";
+
 interface ObjectResponse {
     success: boolean,
     msg: string,
     data: []
+}
+
+export class ResponseUtil {
+    static sendResponse<T>(
+        res: ResponseUtil,
+        message: string,
+        data: T,
+        statusCode = 200
+    ): Response<T> {
+        return res.status(statusCode).send({
+            success: true,
+            code: statusCode,
+            message,
+            data,
+        });
+    }
+
+    static sendError(
+        res: Response,
+        message: string,
+        statusCode = 500,
+        errors: any = null
+    ): Response {
+        return res.status(statusCode).send({
+            success: false,
+            code: statusCode,
+            message,
+            errors,
+        });
+    }
 }
